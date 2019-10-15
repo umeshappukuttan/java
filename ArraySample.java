ec42d392f0d8d0858846a04e9c287994437f2caf
Umesh Appukuttan <u.appukuttantesting@gmail.com>
2019-10-15 08:47:30 +0530

    arraysample-java-files

---
+++ ArraySample.java
@@ -0,0 +1,15 @@
+
+public class ArraySample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int ary[] = {10,20,30,40,50};
+		
+		for(int i=0;i<ary.length;i++)
+		{
+		System.out.println("The Number: "+ary[i]);
+		}
+	}
+
+}
