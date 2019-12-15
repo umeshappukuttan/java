14a2e557a5215317a91a5ba94348ca6fbd15eaee
Umesh Appukuttan <u.appukuttantesting@gmail.com>
2019-12-15 08:47:30 +0530

    loopsample-java-files

---
+++ ForLoopSample.java
@@ -0,0 +1,17 @@
+
+public class ForLoopSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=1;i<=10;i++){
+			
+			System.out.println("Value of i:"+i );
+			//i=i+1;
+			System.out.println("Value of i is :"+i );
+			System.out.print("\n");
+		}
+
+	}
+
+}
