fac3917343ff9d1c9fd32c153f92d1734ff091da
Umesh Appukuttan <u.appukuttantesting@gmail.com>
2020-01-15 08:47:30 +0530

    printsample-java-files

---
+++ PrintDataType.java
@@ -0,0 +1,34 @@
+
+public class PrintDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Byte Variable Printout
+		byte bt = -123;
+		System.out.println("Byte Variable "+ bt);
+		
+		
+		//Integer Variable Printout
+		int Num = 100;
+		System.out.println("Integer Variable "+Num);
+		
+		//String Variable Printout
+		String Str = "Hello World";
+		System.out.println("String Variable "+Str);
+				
+		//Float Variable Printout
+		float flt = 45.5123f;
+		System.out.println("Float Variable "+flt);
+				
+		//Double Variable Printout
+		double dbl = 45.546464;
+		System.out.println("Double Variable "+dbl);
+		
+		//Boolean Variable Printout
+		Boolean bln = true;
+		System.out.println("Boolean Variable "+bln);
+		
+	}
+
+}
